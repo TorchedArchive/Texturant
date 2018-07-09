05ee4dfecec00d7736095da02176d25a19e921e0
Carl Williams <38820196+SamuraiStacks@users.noreply.github.com>
2018-07-09 14:57:54 -0400

    the texturants

---
+++ bin/food.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+const list = require("prompt-list")
+const orderFrom = new list({
+    name: "orderFrom",
+    message: "Where would I like to go today?",
+    choices: [
+        "MacDonalds",
+        {name: "Burger King", disabled: "Not done yet"},
+        {name: "Wendy's", disabled: "Not done yet"},
+        {name: "Chick-fill-A", disabled: "Not done yet"}
+    ]
+})
+
+orderFrom.ask(function(choice) {
+    try {
+        let theTexturant = require(`./texturants/${choice.toLowerCase()}.js`)
+        theTexturant.run(list)
+    } catch (err) {
+        console.log(err)
+    }
+})
---
+++ bin/texturants/macdonalds.js
@@ -0,0 +1,67 @@
+exports.run = (list) => {
+const checkbox = require("prompt-checkbox")
+const clear = require("clear")
+    clear()
+    const MacDonalds = new list({
+        name: "macdonalds",
+        message: "Welcome to MacDonalds! Select what category of food you want please!",
+        choices: [
+            "Breakfast",
+            "test2"
+        ]
+    });
+    MacDonalds.ask(function(ans) {
+        switch (ans) {
+            case "Breakfast":
+                let MCDBreakfast = new checkbox({
+                    name: "macdonalds_breakfast",
+                    message: "You have selected the Breakfast category! Please select which item(s) you would like with *spacebar* and confirm with *Enter*.",
+                    choices: [
+                        "Fruit and Maple Oatmeal",
+                        "Fruit N' Yogurt Parfait",
+                        "Egg McMuffin",
+                        "Egg White Delight McMuffin",
+                        "Sausage McMuffin", 
+                        "Sausage McMufin With Egg",
+                        "Bacon, Egg and Cheese Biscuit"
+                    ]
+                })
+                MCDBreakfast.ask(function(ans) {
+                    console.log(`Susan > I will get you your ${ans.length > 1 ? `${ans.slice(0, ans.length - 1).join(", ")} and ${ans.slice(-1).pop()}` : ans.join(", ")}. Pull-up to the next window`)
+                    setTimeout(() => {
+                        console.log("Susan > Here is your order!")
+                    }, 5000);
+                    setTimeout(() => {
+                        let speech = new list({
+                            name: "speech",
+                            message: "(What should I say?)",
+                            choices: [
+                                "Thanks! Have a nice day!",
+                                "Yeah whatever..",
+                                "Okay bye."
+                            ]
+                        })
+                        speech.ask(function(ans) {
+                            console.log(`You > ${ans}`)
+                            switch (ans) {
+                                case "Thanks! Have a nice day!":
+                                    console.log("Susan > You too! Come again!")
+                                    break;
+                                case "Yeah whatever..":
+                                    console.log("Susan > So uh bye")
+                                    break;
+                                case "Okay bye.":
+                                    console.log("Susan > Later...")
+                                    break;
+                                default:
+                                    break;
+                            }
+                        })
+                    }, 6000);
+                })
+                break;
+            default:
+                break;
+        }
+    });
+}
