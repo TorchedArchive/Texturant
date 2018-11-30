226c33fa6b7df09cb018c21509448152e31cb36e
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-11-30 19:54:44 -0400

    mmlol

--- bin/utils/Utils.js
+++ bin/utils/Utils.js
@@ -3,6 +3,7 @@ class Utils {
         this.main = require("./mainUtils")
         this.banners = require("./ascii-banners")
         this.functions = require("./mainjsFunctions")
+        this.selection = require("./selection/selection.js")
     }
 }
 module.exports = Utils
