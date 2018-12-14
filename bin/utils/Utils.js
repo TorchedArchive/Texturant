a819ef111b49ba09c804c19c6c625cdb21dc2215
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-12-14 19:56:54 -0400

    remove selection

--- bin/utils/Utils.js
+++ bin/utils/Utils.js
@@ -3,7 +3,6 @@ class Utils {
         this.main = require("./mainUtils")
         this.banners = require("./ascii-banners")
         this.functions = require("./mainjsFunctions")
-        this.selection = require("./selection/selection.js")
     }
 }
 module.exports = Utils
