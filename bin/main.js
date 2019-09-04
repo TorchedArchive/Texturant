adbe42c84d1a16582fcbbfd726e808289337f087
SamuraiStacks <38820196+SamuraiStacks@users.noreply.github.com>
2019-09-04 10:55:37 -0400

    === is to be used not =

--- bin/main.js
+++ bin/main.js
@@ -14,7 +14,14 @@ inquirer.prompt({
         "No"
     ]
 }).then(a => {
-    if (a.fullscreen = "Yes") {
-        console.log("ok")
+    if (a.fullscreen === "Yes") {
+        process.stdout.write('\033c');
+        // Utils.functions.mainMenu()
+    } else {
+        process.stdout.write('\033c');
+        console.log("Things might look weird if you are not in fullscreen.")
+        setTimeout(() => {
+            // Utils.functions.mainMenu()
+        }, 4000)
     }
 })
